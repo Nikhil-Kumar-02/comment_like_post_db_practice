0b10ddb4dd9fbc31095fb2c987a02ec09bd5cde9
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-02 08:25:41 +0530

    added logic to get all comments from data base and get individually by comment id

--- controller/commentController.js
+++ controller/commentController.js
@@ -20,4 +20,40 @@ const addComment = async (req,res) => {
     }
 }
 
-module.exports = addComment;
+const getCommentByid = async (req,res) => {
+    try {
+        const commentid = req.params.commentid;
+        const result = await Comment.findById(commentid);
+        res.status(200).json({
+            message : 'data found sucessfully in db',
+            data : result
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : "data not fetched from db",
+            description : error
+        })
+    }
+}
+
+const getAllComment = async (req,res) => {
+    try {
+        const result = await Comment.find({});
+        res.status(200).json({
+            message : 'data fetched from db',
+            data : result
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : "data not fetched from db",
+            description : error
+        })
+    }
+}
+
+
+module.exports = {
+    addComment,
+    getAllComment,
+    getCommentByid
+};
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const addComment = require('../../controller/commentController');
+const {addComment , getAllComment , getCommentByid} = require('../../controller/commentController');
 const router = express.Router();
 
 router.post('/comment' , addComment);
-
+router.get('/getComments' , getAllComment);
+router.get('/getComment/:commentid' , getCommentByid);
 
 module.exports = router;
