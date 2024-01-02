1da1616490f86f020f5764991e523c1524572854
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-02 09:06:13 +0530

    added all logic to deal with the post as of now

---
+++ controller/postController.js
@@ -0,0 +1,79 @@
+const Post = require('../models/post');
+
+const addPost = async (req,res) => {
+    try {
+        const data = {
+            "title" : req.body.title,
+            "body" : req.body.body
+        }
+        console.log('this is the input created' , data);
+        const result = await Post.create(data);
+        res.status(200).json({
+            message : 'data inserted sucessfully into db',
+            data : result
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : "data not added to db",
+            description : error
+        })
+    }
+}
+
+const getPostByid = async (req,res) => {
+    try {
+        const postid = req.params.postid;
+        const result = await Post.findById(postid);
+        res.status(200).json({
+            message : 'data found sucessfully in db',
+            data : result
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : "data not fetched from db",
+            description : error
+        })
+    }
+}
+
+const getAllPost = async (req,res) => {
+    try {
+        const result = await Post.find({});
+        res.status(200).json({
+            message : 'data fetched from db',
+            data : result
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : "data not fetched from db",
+            description : error
+        })
+    }
+}
+
+const updatePost = async (req,res) => {
+    try {
+        const postid = req.params.postid;
+        const {title , body} = req.body;
+        console.log(postid , {title , body});
+        const result = await Post.findByIdAndUpdate({_id : postid} , {title,body} , {new : true});
+        console.log(result)
+        res.status(200).json({
+            message : 'data updated in db',
+            data : result
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : "data not fetched from db",
+            description : error
+        })
+    }
+}
+
+
+module.exports = {
+    addPost,
+    getAllPost,
+    getPostByid,
+    updatePost
+};
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {addComment , getAllComment , getCommentByid , updateComment} = require('../../controller/commentController');
+const { addPost, getAllPost, getPostByid, updatePost } = require('../../controller/postController');
 const router = express.Router();
 
 router.post('/comment' , addComment);
@@ -7,4 +8,8 @@ router.get('/getComments' , getAllComment);
 router.get('/getComment/:commentid' , getCommentByid);
 router.put('/updateComment/:commentid' , updateComment);
 
+router.post('/post' , addPost);
+router.get('/getPosts' , getAllPost);
+router.get('/getPost/:postid' , getPostByid);
+router.put('/updatePost/:postid' , updatePost);
 module.exports = router;
